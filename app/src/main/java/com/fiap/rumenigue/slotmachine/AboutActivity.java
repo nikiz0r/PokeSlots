13776e1a3ea6484e1c7bf34793c0b5fbdd5cffe3
Rumenigue Nogueira da Silva <niguerag@hotmail.com>
2017-05-26 08:52:16 -0300

    Back button in aboutActivity

--- app/src/main/java/com/fiap/rumenigue/slotmachine/AboutActivity.java
+++ app/src/main/java/com/fiap/rumenigue/slotmachine/AboutActivity.java
@@ -3,6 +3,7 @@ package com.fiap.rumenigue.slotmachine;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -15,5 +16,9 @@ public class AboutActivity extends AppCompatActivity {
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/poke.ttf");
         TextView tvLogo = (TextView) findViewById(R.id.tvLogo);
         tvLogo.setTypeface(font);
+    }
+
+    public void back(View view) {
+        this.finish();
     }
 }
